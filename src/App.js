def98df1fabc68e23baabb70634b1974ae4bf0e6
Gonzalo Marino <gmarino@fi.uba.ar>
2022-11-26 13:56:46 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './assests/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Find My Team.
+        </h1>
+        <h5>
+          Work in progress.
+        </h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
