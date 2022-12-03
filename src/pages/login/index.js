d5440514ccf328b6c0e0f2a71cbb310fb9559dae
Gonzalo Marino <gmarino@fi.uba.ar>
2022-12-03 15:20:49 -0300

    wip mainpage

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,8 +1,25 @@
 import './style.css';
 import pana from "../../assests/pana.svg";
 import Logo from "../../components/logo";
+import {useState} from "react";
+import {Eye, EyeSlash} from "iconsax-react";
 
 function Login() {
+    const [email, setEmail] = useState([]);
+    const [password, setPassword] = useState([]);
+    const [passwordShown, setPasswordShown] = useState(false);
+    const setEmailHandler = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const setPasswordHandler = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown);
+    };
+
     return (
         <div className="container">
             <Logo/>
@@ -13,22 +30,48 @@ function Login() {
                     </text>
                     <img src={pana} className="pana-style" alt="logo"/>
                 </div>
-                <div className="form">
-                    <text>
-                        Encuentra tu equipo ideal de manera sencilla y rápida
+                <div className="form-container">
+                    <text className="form-text">
+                        Únete y forma parte de nuestra comunidad
                     </text>
-                    <text>
-                        Encuentra tu equipo ideal de manera sencilla y rápida
-                    </text>
-                    <text>
-                        Encuentra tu equipo ideal de manera sencilla y rápida
-                    </text>
-                    <text>
-                        Encuentra tu equipo ideal de manera sencilla y rápida
-                    </text>
-                    <text>
-                        Encuentra tu equipo ideal de manera sencilla y rápida
-                    </text>
+                    <from className="form">
+                        <div className="label">
+                            <label>
+                                Email:
+                                <div className="form-input">
+                                    <input type="text" value={email} className="input" onChange={setEmailHandler}/>
+                                </div>
+                            </label>
+                        </div>
+                        <div className="label">
+                            <label>
+                                Password:
+                                <div className="form-input">
+                                    <input type={passwordShown ? "text" : "password"} value={password}
+                                           className="input"
+                                           onChange={setPasswordHandler}/>
+                                    {passwordShown ?
+                                        <Eye className="password-button" color="#B1B1B1" variant="outline" size={24}
+                                             onClick={togglePassword}/> :
+                                        <EyeSlash className="password-button" color="#B1B1B1" variant="outline"
+                                                  size={24}
+                                                  onClick={togglePassword}/>}
+                                </div>
+                            </label>
+                        </div>
+                    </from>
+                    <div className="button-container">
+                        <button className="button-style" onClick={() => {
+                            console.log("unirse")
+                        }}>Unirse
+                        </button>
+                        <div className="container-button-login">
+                            <text>
+                                ¿Ya tienes una cuenta?
+                            </text>
+                            <button className="login">Inicia Sesión</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
