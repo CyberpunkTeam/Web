06afa2584a2efcf31c0b497948064253d17c4046
GonzaM21 <gonzalo.marino@fi.uba.ar>
2023-01-08 17:21:36 -0300

    add sidebar mobile

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,10 +1,11 @@
 import logo from '../../assests/fmt-logo.svg';
 import './style.css';
+import {isMobile} from "react-device-detect";
 
 function Loading() {
     return (
-        <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
+        <div className={"App-header"}>
+            <img src={logo} className={isMobile ? "App-logo-mobile" : "App-logo"} alt="logo"/>
         </div>
     );
 }
