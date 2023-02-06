4d6b2e6dedccc8853eae2e14230bd54cb0dce00f
GonzaM21 <gonzalo.marino@fi.uba.ar>
2023-02-06 15:51:42 -0300

    add data dictonary

---
+++ src/config/dictonary.js
@@ -0,0 +1,29 @@
+const languages = ["Python", "Java", "JavaScript", "C++", "Go", "TypeScript", "PHP", "Ruby", "C", "C#", "Shell",
+    "Nix", "HTML", "Scala", "Rust", "Kotlin", "Swift", "Jupyter Notebook", "Dart", "DM", "HCL", "Lua", "SystemVerilog",
+    "CSS", "Perl", "Groovy", "Makefile", "Objective-C", "CodeQL", "SCSS", "Elixir", "OCaml", "Vue", "Haskell", "Starlark",
+    "Lean", "Dockerfile", "Erlang", "Smarty", "Emacs Lisp", "Jinja", "PowerShell", "JSON", "Clojure", "Julia", "MLIR",
+    "Verilog", "Vim Script", "Roff", "CoffeeScript", "Assembly", "WebAssembly", "Puppet", "F#", "R", "Jsonnet", "GAP",
+    "Bicep", "MATLAB", "BitBake", "Visual Basic .NET", "PLpgSQL", "YAML", "ANTLR", "hoon", "Markdown", "TeX", "Fortran",
+    "Twig", "Cython", "ZAP", "Cuda", "SourcePawn", "ColdFusion", "GLSL", "Sass", "Nunjucks", "XSLT", "Mustache",
+    "Common Lisp", "SWIG", "Smalltalk", "Less", "Elm", "Gherkin", "Coq", "Vala", "CMake", "PostScript",
+    "Haxe", "LLVM", "F*", "Stylus", "Crystal", "Raku", "Blade", "Batchfile", "Slim", "Handlebars", "Pascal"]
+
+const frameworks = {
+    "Java": ["Spring"],
+    "Python": ["Django", "Flask", "FastAPI"],
+    "Ruby": ["Ruby on Rails"],
+    "JavaScript": ["React JS", "Node JS", "Vue JS", "Express", "Ember JS", "Meteor JS", "Expo", "React Native"],
+    "TypeScript": ["Next JS", "Angular"],
+    "C#": ["ASP.NET"],
+    "PHP": ["CakePHP", "Laravel"],
+    "Go": ["Gin", "Beego", "Echo", "Fiber"],
+    "Dart": ["Flutter"]
+}
+
+const platforms = ["Wix", "Squarespace", "GoDaddy", "Weebly", "Zyro", "Jimdo Creator", "WordPress.com", "SITE123", "Strikingly", "1&1 IONOS 2"]
+
+const cloudProviders = ["AWS", "Microsoft Azure", "GCP", "Alibaba Cloud", "Oracle Cloud", "IBM Cloud", "Tencent Cloud", "OVHcloud", "DigitalOcean", "Linode", "Salesforce", "SAP"]
+
+const dataBases = ["Oracle", "MySQL", "Microsoft SQL Server", "PostgreSQL", "MongoDB", "Redis", "Elasticsearch", "SQLite", "Cassandra", "Splunk", "MariaDB", "Teradata", "Hive", "Solr", "HBase", "Amazon DynamoDB", "Neo4j", "Couchbase", "Memcached", "Microsoft Azure SQL Database"]
+
+const methodologies = ["Agile Software Development", "Scrum", "FDD", "Lean Development", "XP", "Waterfall Model", "Prototype Model", "RAD", "Dynamic Systems Development Model", "Spiral Model", "JAD", "Rational Unified Process", "DevOps Methodology", "Adaptive Software Development", "BDD"]
