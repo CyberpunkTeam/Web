680ad4f502acb208692f7a48169a82c4a4ff6ac8
GonzaM21 <gonzalo.marino@fi.uba.ar>
2023-03-04 17:30:23 -0300

    add pagination

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -10,6 +10,7 @@ import Loading from "../../components/loading";
 import {isMobile} from "react-device-detect";
 import AppContext from "../../utils/AppContext";
 import ProjectTileMobileComponent from "../../components/ProjectTileMobileComponent";
+import ReactPaginate from "react-paginate";
 
 export default function ProjectsScreen() {
     const navigate = useNavigate();
@@ -19,11 +20,17 @@ export default function ProjectsScreen() {
     const [team, setTeam] = useState(true);
     const [time, setTime] = useState(Date.now());
     const [shake, setShake] = useState(false);
+    const [index, setIndex] = useState(0);
+    const pageCount = Math.ceil(projects.length / 10);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * 10) % projects.length;
+        setIndex(newOffset);
+    };
 
     const animate = () => {
         setShake(true);
         setTimeout(() => setShake(false), 500);
-
     }
 
     const change = () => {
@@ -64,11 +71,14 @@ export default function ProjectsScreen() {
                     <div className={isMobile ? "projects-cover-title-reduced-mobile" : "projects-cover-title-reduced"}>
                         Create a project that fits your preferences
                     </div>
-                    <img src={logo} className={isMobile ? "pana-projects-style-mobile" : "pana-projects-style-reduced"} alt="logo"/>
-                    <button className={isMobile ? "createProjectButtonCoverMobile" : "createProjectButtonCover"} onClick={() => {
-                        navigate("/new/projects/type")
-                    }}>
-                        <AddCircle color="#FAFAFA" variant="Bold" size={isMobile ? 48 : 24} className={isMobile ? "iconMobile" : "icon"}/>
+                    <img src={logo} className={isMobile ? "pana-projects-style-mobile" : "pana-projects-style-reduced"}
+                         alt="logo"/>
+                    <button className={isMobile ? "createProjectButtonCoverMobile" : "createProjectButtonCover"}
+                            onClick={() => {
+                                navigate("/new/projects/type")
+                            }}>
+                        <AddCircle color="#FAFAFA" variant="Bold" size={isMobile ? 48 : 24}
+                                   className={isMobile ? "iconMobile" : "icon"}/>
                         Create Project
                     </button>
                 </div>
@@ -125,9 +135,20 @@ export default function ProjectsScreen() {
             <div className={isMobile ? "profile-screen-mobile" : "projects-screen"}>
                 {isMobile || context.size ? coverMobile() : cover()}
                 <div className="projects-container">
-                    {projects.map((value) => {
+                    {projects.slice(index, 10 + (index)).map((value) => {
                         return <ProjectTileMobileComponent key={value.pid} data={value}/>
                     })}
+                    <div className={"pagination"}>
+                        <ReactPaginate
+                            className={"pagination"}
+                            nextLabel={<ArrowCircleRight size="24" color={index + 10 > projects.length ? "#E3E3E3" : "#014751"} className={"pagination-icon"}/>}
+                            onPageChange={handlePageClick}
+                            pageRangeDisplayed={10}
+                            pageCount={pageCount}
+                            previousLabel={<ArrowCircleLeft size="24" color={index === 0 ? "#E3E3E3" : "#014751"} className={"pagination-icon"}/>}
+                            renderOnZeroPageCount={null}
+                        />
+                    </div>
                 </div>
             </div>
             <SearchBar/>
