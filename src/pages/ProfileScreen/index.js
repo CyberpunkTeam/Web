7e51488ac947109b901abbd2126a85bed4442744
GonzaM21 <gonzalo.marino@fi.uba.ar>
2023-05-04 22:14:50 -0300

    fixes

--- src/pages/ProfileScreen/index.js
+++ src/pages/ProfileScreen/index.js
@@ -3,7 +3,7 @@ import SideBar from "../../components/SideBar";
 import AppContext from "../../utils/AppContext";
 import React, {useContext, useEffect, useState} from "react";
 import NotFound from "../NotFound";
-import {AddCircle, Edit, ArrowForward, User, Notepad2, Message} from "iconsax-react";
+import {AddCircle, Edit, ArrowForward, User, Notepad2, Message, Share} from "iconsax-react";
 import Modal from 'react-modal';
 import {useNavigate, useParams} from "react-router-dom";
 import {followUser, getProfile} from "../../services/userService";
@@ -208,15 +208,11 @@ function ProfileScreen() {
         }
 
         return (
-            <button
-                className={isMobile ? "followButtonMobile" : "followReducedButton"}
-                disabled={followButtonStatus}
-                onClick={recommendUserButton}>
-                {followButtonStatus ?
-                    <i className="fa fa-circle-o-notch fa-spin"></i> :
-                    <ArrowForward color="#FAFAFA" size={isMobile ? 48 : 24}/>
-                }
-            </button>
+            <div className="cover-recommend-buttons" onClick={recommendUserButton}>
+                <div className={isMobile ? "edit-button-mobile" : "edit-button"}>
+                    <Share size={isMobile ? 48 : 24} color="#014751"/>
+                </div>
+            </div>
         )
     }
 
